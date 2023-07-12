1ce91a65f22ca90afe7d4766033419261876d6c3
TXT-80 <txt.hackathon@TXTs-MBP-30.lan>
2023-07-12 01:32:19 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import PokemonCard from "./Pages/PokemonCard";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [pokemonList, setPokemonList] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const pokemon = await axios.get(
+          "http://localhost:3000/catch/all-pokemons"
+        );
+        setPokemonList(pokemon.data);
+      } catch (err) {
+        console.log(err);
+      }
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="pokedexContainer">
+      <h1>Pokedex</h1>
+      {loading ? (
+        <h1>Pokedex Loading Pokemon..." </h1>
+      ) : (
+        pokemonList.map((pokemon) => <PokemonCard pokemon={pokemon}/>)
+      ) }
     </div>
   );
 }
---
+++ src/Pages/PokemonCard.js
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "../App.css";
+
+const typeColors = {};
+
+function PokemonCard({ pokemon }) {
+    const [loading, setLoading] = useState(false);
+    const [pokeDetails, setPokeDetails] = useState({});
+
+    useEffect(() => {
+        const setSinglePokemonData = async () => {
+            setLoading(true);
+            try {
+                const data = await axios.get(pokemon.url);
+                setPokeDetails(data)
+            } catch(error) {
+                console.log(error);
+            }
+            setLoading(false);
+        };
+
+        setSinglePokemonData();
+    }, []);
+  return (
+    // <>
+    // <img src={pokeDetails.data?.sprites.front_default} />
+    // {console.log(pokeDetails.data)}
+    // {/* </>
+    <div className="card">
+      {/*create these class names in app.css*/}
+      <div className="card_image">
+        <img src={pokeDetails.data?.sprites.front_default} />
+      </div>
+      <div className="card_name">{pokeDetails.data?.name}</div>
+      <div className="card_type">{pokeDetails.data?.type}</div>
+       
+
+      {/* Card info such as weight height etc */}
+      <div className="Card_info">
+        <div className="card_data_weight">
+          <p>Weight</p>
+          <p>{pokeDetails.data?.weight}</p>
+        </div>
+        <div className="card_height">
+          <p>Height</p>
+          <p>{pokeDetails.data?.height}</p>
+        </div>
+        <div className="card_ability">
+          <p>{pokeDetails.data?.abilities[0].ability.name}</p>
+        </div>
+      </div>
+    </div> 
+  );
+}
+export default PokemonCard;
